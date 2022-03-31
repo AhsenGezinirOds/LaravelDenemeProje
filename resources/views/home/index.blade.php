06a044b4d186fd11747d4322a9a2c339126cd778
ahsengznr <ahsen.gezinir@odsdanismanlik.com>
2022-03-31 11:44:54 +0300

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main')
+ 
+@section('title', 'Sub Page Title')
+ 
+@section('sidebar')
+    <div class="col-sm-4 col-lg-2">        
+        <ul>
+            <li>Bilgisayar</li>
+            <li>Elektronik</li>
+            <li>Ev Eşyaları</li>
+        </ul>           
+    </div>
+@endsection
+ 
+@section('content')
+    <div class="col-sm-8 col-lg-10">
+    <h1>Slider</h1>
+    zzzzzzz
+    <br>
+    bbbbbbbbb <br>
+
+    <h1>İçeriklerimiz<h1>
+    
+    ddddddddddd<br>
+    kkkkkkkkkkk<br>
+    </div>
+
+  
+@endsection
